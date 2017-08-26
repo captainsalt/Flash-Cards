1f890d8b995fdaa9d748b5bf4660282ec6840f2f
David Wright <dontbothertheking@gmail.com>
2017-08-26 00:25:40 -0500

    Added basic card and simple changing between front and back of cards

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -1,8 +1,42 @@
 ﻿$(document).ready(() => {
     var card = new ClozeCard("David", "David Wright");
     console.log(card.partial);
+
+    GenerateBasicCard("David", "Wright");
+
+    //flip basic card
+    $(".basic-card").click(e => {
+        var card = $(e.currentTarget);
+        var context = card.children(".context");
+        var displayFront = context.data("displayFront");
+
+        if (displayFront) {
+            context.html(context.data("back"));
+            context.data("displayFront", false);
+            
+        } else if (!displayFront) {
+            context.html(context.data("front"));
+            context.data("displayFront", true);
+        }
+    });
 });
 
-function GenerateBasicCard(front, back) {
+function GenerateClozeCard(cloze, text) {
+    var card = new ClozeCard(cloze, text);
+}
 
+function GenerateBasicCard(front, back) {
+    var cardInfo = new BasicCard(front, back);
+
+    var card = $(`<div>`).addClass("basic-card");
+
+    var context = $(`<p>`)
+        .addClass("context")
+        .data("front", cardInfo.front)
+        .data("back", cardInfo.back)
+        .data("displayFront", true)
+        .html(cardInfo.front);
+
+    card.append(context);
+    $("#cards").append(card);
 }
