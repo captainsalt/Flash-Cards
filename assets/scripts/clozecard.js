ef45e045fdcac5aabc922b04214dac4eb5e11892
David Wright <dontbothertheking@gmail.com>
2017-08-25 23:02:29 -0500

    added cloze card

---
+++ assets/scripts/clozecard.js
@@ -0,0 +1,15 @@
+function ClozeCard(cloze, fullText) {
+    if (!(this instanceof ClozeCard))
+        return new ClozeCard(cloze, partial, fullText);
+
+    this.cloze = cloze;
+    this.fullText = checkCloze();
+    this.partial = this.fullText.replace(cloze, "...");
+    
+    function checkCloze() {
+        if (fullText.includes(cloze))
+            return fullText;
+        else
+            throw new Error("Full text does not contain close\nNote: Case is important!");
+    }    
+}
